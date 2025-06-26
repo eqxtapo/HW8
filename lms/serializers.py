c56d254890858ce132bb6dc3e09a1e15b534e337
unknown <extapo@yandex>
2025-06-26 19:32:27 +0700

    lms

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,9 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lessons_quantity = SerializerMethodField()
+    lessons_info = SerializerMethodField()
+
+    def get_lessons_quantity(self, obj):
+        return obj.lesson_set.count()
+
+    def get_lessons_info(self, obj):
+        lessons = obj.lesson_set.all()
+        return LessonSerializer(lessons, many=True).data
+
     class Meta:
         model = Course
         fields = "__all__"
--- lms/views.py
+++ lms/views.py
@@ -1,10 +1,6 @@
-from rest_framework.generics import (
-    CreateAPIView,
-    DestroyAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-    UpdateAPIView,
-)
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, RetrieveAPIView,
+                                     UpdateAPIView)
 from rest_framework.viewsets import ModelViewSet
 
 from lms.models import Course, Lesson
